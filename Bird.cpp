10f9556b06c66b92f62e669a61191b142556328b
Ivan Yarovitsin <83675876+Yarovitsin@users.noreply.github.com>
2024-12-16 13:26:15 +0300

    Create Bird.cpp

---
+++ Bird.cpp
@@ -0,0 +1,35 @@
+#include "Bird.h"
+
+Bird::Bird() {}
+
+Bird::Bird(char* input_name, char* input_species, int input_age) {
+	strcpy(name, input_name);
+	strcpy(species, input_species);
+	age = input_age;
+}
+
+char* Bird::bird_info() {
+	char string[200];
+	sprintf(string, "Name: %s, species: %s, age: %d\n", name, species, age);
+	return string;
+}
+
+Bird Bird::operator+(Bird bird) {
+	Bird new_bird;
+	char new_bird_name[20];
+	char new_bird_species[50];
+	sprintf(new_bird_name, "%s%s", name, bird.name);
+	sprintf(new_bird_species, "%s%s", species, bird.species);
+	strcpy(new_bird.name, new_bird_name);
+	strcpy(new_bird.species, new_bird_species);
+	return new_bird;
+}
+
+bool Bird::operator<(Bird bird) {
+	if (age < bird.age) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
