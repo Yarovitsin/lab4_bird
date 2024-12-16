2bf3d38bacac70a83de2c216e135a90e9ee3ae29
Ivan Yarovitsin <83675876+Yarovitsin@users.noreply.github.com>
2024-12-16 13:42:33 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@ void main() {
 	int bird2_age = 0;
 
 	printf("Enter first bird's name, species and age:\n");
-	scanf("%s%s%d", bird1_name, bird1_species, bird1_age);
+	scanf("%s%s%d", &bird1_name, &bird1_species, &bird1_age);
 	printf("Enter second bird's name, species and age:\n");
-	scanf("%s%s%d", bird2_name, bird2_species, bird2_age);
+	scanf("%s%s%d", &bird2_name, &bird2_species, &bird2_age);
 
 	Bird bird1(bird1_name, bird1_species, bird1_age);
 	Bird bird2(bird2_name, bird2_species, bird2_age);
@@ -18,12 +18,14 @@ void main() {
 	printf("%s", bird2.bird_info());
 
 	if (bird1 < bird2) {
-		printf("The first bird is younger than the second bird");
+		printf("The first bird is younger than the second bird\n");
 	}
 	else {
-		printf("The first bird is of equal age or older than the second bird");
+		printf("The first bird is of equal age or older than the second bird\n");
 	}
 
 	Bird bird3 = bird1 + bird2;
-	printf("Combined bird's info: %s", bird3.bird_info());
+	printf("Combined bird's info: %s\n", bird3.bird_info());
+
+	system("pause");
 }
